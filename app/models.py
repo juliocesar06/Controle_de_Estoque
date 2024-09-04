1daf685ee914f13c4b177c3a354e664e047f3dfb
Julio <julio06cesar11@gmail.com>
2024-09-04 00:51:44 -0300

    aprendendo flask

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from run import Tecido
+#criar tabelas par banco de dados
+add_tecido = [
+    Tecido(cor=str(1001),qtd_mts=1200,qtd_und=8,fornecedor = 'Santanense',tipo='leve'),
+    Tecido(cor=str(1006),qtd_mts=1800,qtd_und=8,fornecedor = 'Santanense',tipo='leve'),
+    Tecido(cor=str(1008),qtd_mts=700,qtd_und=8,fornecedor = 'Santanense',tipo='office'),
+    Tecido(cor=str(1009),qtd_mts=2800,qtd_und=8,fornecedor = 'Santanense',tipo='office'),
+    Tecido(cor=str(1021),qtd_mts=3800,qtd_und=8,fornecedor = 'Santanense',tipo='jeans'),
+    Tecido(cor=str(1028),qtd_mts=800,qtd_und=8,fornecedor = 'Santanense',tipo='forte'),
+    Tecido(cor=str(1043),qtd_mts=200,qtd_und=8,fornecedor = 'Santanense',tipo='forte'),
+    Tecido(cor=str(1009),qtd_mts=300,qtd_und=8,fornecedor = 'Santanense',tipo='leve'),
+    Tecido(cor=str(721),qtd_mts=200,qtd_und=8,fornecedor = 'Santista',tipo='forte'),
+    Tecido(cor=str(704),qtd_mts=1000,qtd_und=8,fornecedor = 'Santista',tipo='leve'), 
+]
